b05a706a5a75ce423f3441ff928d6b7e58d1d8f8
shinjitsue <71762653+shinjitsue@users.noreply.github.com>
2025-01-13 19:56:35 +0800

    feat: 🤝 Add How It Works component and integrate into HomeView

---
+++ src/components/system/HowItWorks.vue
@@ -0,0 +1,76 @@
+<script setup>
+import UsingComputer from '@/components/icons/UsingComputer.vue'
+import ContactPerson from '@/components/icons/ContactPerson.vue'
+import SitBack from '@/components/icons/SitBack.vue'
+</script>
+
+<template>
+  <div class="my-15">
+    <div class="mb-10 text-center">
+      <h1>How It Works</h1>
+      <small class="ps-2">
+        Discover how our platform connects you with the best properties and brokers in Butuan City.
+      </small>
+    </div>
+
+    <v-timeline direction="horizontal" dot-color="surface" fill-dot>
+      <v-timeline-item size="large" elevation="10" class="first-timeline">
+        <template v-slot:icon>
+          <v-icon color="amber-accent-3" size="x-large">mdi-numeric-1</v-icon>
+        </template>
+        <template v-slot:opposite>
+          <div class="text-center">
+            <div class="text-h6 font-weight-black pb-2">
+              Search the kind <br />
+              of property you want
+            </div>
+            <p class="text-medium-emphasis">
+              Use our advanced search tools to find the perfect property that meets your needs and
+              preferences.
+            </p>
+          </div>
+        </template>
+        <div>
+          <UsingComputer />
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item size="large" elevation="10">
+        <template v-slot:icon>
+          <v-icon color="amber-accent-3" size="x-large">mdi-numeric-2</v-icon>
+        </template>
+        <template v-slot:opposite>
+          <ContactPerson />
+        </template>
+        <div class="text-center">
+          <div class="text-h6 font-weight-black pb-2">Contact the broker</div>
+          <p class="text-medium-emphasis">
+            Get in touch with experienced brokers who can provide you with more details and arrange
+            viewings.
+          </p>
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item size="large" elevation="10" class="last-timeline">
+        <template v-slot:icon>
+          <v-icon color="amber-accent-3" size="x-large">mdi-numeric-3</v-icon>
+        </template>
+        <template v-slot:opposite>
+          <div class="text-center">
+            <div class="text-h6 font-weight-black pb-1">
+              Sit back and <br />
+              wait for the good news
+            </div>
+            <p class="text-medium-emphasis">
+              Once you've found your ideal property, our brokers will handle the rest, ensuring a
+              smooth transaction process.
+            </p>
+          </div>
+        </template>
+        <div>
+          <SitBack />
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,7 @@ import { ref, computed } from 'vue'
 import { useThemeStore } from '@/stores/themeStore'
 import AppFooter from '@/components/layout/AppFooter.vue'
 import FeaturedProperties from '@/components/system/properties/FeaturedProperties.vue'
-import UsingComputer from '@/components/icons/UsingComputer.vue'
-import ContactPerson from '@/components/icons/ContactPerson.vue'
-import SitBack from '@/components/icons/SitBack.vue'
+import HowItWorks from '@/components/system/HowItWorks.vue'
 
 const themeStore = useThemeStore()
 const selectedItem = ref(0)
@@ -153,75 +151,7 @@ const items = [
     <v-container>
       <FeaturedProperties />
 
-      <div class="my-15">
-        <div class="mb-10">
-          <h1><v-icon>mdi-minus</v-icon> How It Works</h1>
-          <small class="ps-2">
-            Discover how our platform connects you with the best properties and brokers in Butuan
-            City.
-          </small>
-        </div>
-
-        <v-timeline direction="horizontal" dot-color="surface" fill-dot>
-          <v-timeline-item size="large" elevation="10" class="first-timeline">
-            <template v-slot:icon>
-              <v-icon color="amber-accent-3" size="x-large">mdi-numeric-1</v-icon>
-            </template>
-            <template v-slot:opposite>
-              <div class="text-center">
-                <div class="text-h6 font-weight-black pb-2">
-                  Search the kind <br />
-                  of property you want
-                </div>
-                <p class="text-medium-emphasis">
-                  Use our advanced search tools to find the perfect property that meets your needs
-                  and preferences.
-                </p>
-              </div>
-            </template>
-            <div>
-              <UsingComputer />
-            </div>
-          </v-timeline-item>
-
-          <v-timeline-item size="large" elevation="10">
-            <template v-slot:icon>
-              <v-icon color="amber-accent-3" size="x-large">mdi-numeric-2</v-icon>
-            </template>
-            <template v-slot:opposite>
-              <ContactPerson />
-            </template>
-            <div class="text-center">
-              <div class="text-h6 font-weight-black pb-2">Contact the broker</div>
-              <p class="text-medium-emphasis">
-                Get in touch with experienced brokers who can provide you with more details and
-                arrange viewings.
-              </p>
-            </div>
-          </v-timeline-item>
-
-          <v-timeline-item size="large" elevation="10" class="last-timeline">
-            <template v-slot:icon>
-              <v-icon color="amber-accent-3" size="x-large">mdi-numeric-3</v-icon>
-            </template>
-            <template v-slot:opposite>
-              <div class="text-center">
-                <div class="text-h6 font-weight-black pb-1">
-                  Sit back and <br />
-                  wait for the good news
-                </div>
-                <p class="text-medium-emphasis">
-                  Once you've found your ideal property, our brokers will handle the rest, ensuring
-                  a smooth transaction process.
-                </p>
-              </div>
-            </template>
-            <div>
-              <SitBack />
-            </div>
-          </v-timeline-item>
-        </v-timeline>
-      </div>
+      <HowItWorks />
     </v-container>
 
     <AppFooter />
