632adce720ee604314341702439cf34d6ef306b2
shinjitsue <71762653+shinjitsue@users.noreply.github.com>
2025-01-20 10:12:37 +0800

    feat: 🔥 add shadow effect to SideNavigation on hover for improved UI

--- src/components/layout/navigation/SideNavigation.vue
+++ src/components/layout/navigation/SideNavigation.vue
@@ -54,6 +54,7 @@ onMounted(() => {
     v-model="drawer"
     :rail="props.rail"
     :expand-on-hover="props.expandOnHover"
+    :class="{ 'dh-shadow': props.expandOnHover }"
     permanent
     floating
   >
@@ -143,3 +144,12 @@ onMounted(() => {
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<style scoped>
+.dh-shadow:hover {
+  box-shadow:
+    0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+    0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+    0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+}
+</style>
